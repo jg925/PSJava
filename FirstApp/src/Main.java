49b524799dcc71f442903cbdd4765a1405d387c7
jg925 <jg925@cornell.edu>
2022-06-17 14:38:40 -0700

    first commit

---
+++ FirstApp/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    /*
+     * Block comment
+     */
+    public static void main(String[] args) {
+        // Line comment
+        System.out.println("First line from app");
+        System.out.println("Second line from app");
+        System.out.println("Third line from app");
+    }
+}
